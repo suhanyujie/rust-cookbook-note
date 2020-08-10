bf33036042b499a4b5ebc321cf92aefa96100ec7
suhanyujie <suhanyujie@qq.com>
2020-08-10 09:47:18 +0800

    着手尝试 shell 排序

--- src/notes/sort.rs
+++ src/notes/sort.rs
@@ -10,12 +10,15 @@ pub fn simple_bubble(arr: Vec<i32>) -> Vec<i32> {
 }
 
 /// 插入排序
+/// such as: 2,**4**,12,7, 9
+/// 待排序的元素是从下标为 1 开始的所有数值
 pub fn insert(arr: &mut Vec<i32>) {
     let arr_l = arr.len();
     for i in 1..arr_l {
         let mut j = i;
         while j >= 1 {
             if arr[j] < arr[j - 1] {
+                // 交换数据
                 let tmp = arr[j - 1];
                 arr[j - 1] = arr[j];
                 arr[j] = tmp;
@@ -25,6 +28,26 @@ pub fn insert(arr: &mut Vec<i32>) {
             j -= 1;
         }
     }
+}
+
+/// 希尔排序
+/// 插入排序的改良版本
+pub fn shell_sort(arr: &Vec<i32>) {
+    // 获取增长量
+    let h = get_shell_addition_rate(arr.len());
+    // 排序
+    while h >= 1 {
+
+    }
+}
+
+/// 获取希尔排序的增长量
+fn get_shell_addition_rate(l: usize) -> usize {
+    let mut h = 1;
+    while h < l / 2 {
+        h = 2 * h + 1;
+    }
+    h
 }
 
 #[cfg(test)]
