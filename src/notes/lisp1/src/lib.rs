2abea1597b17e3c28c1292af1ba1aac1c628123a
suhanyujie <suhanyujie@qq.com>
2021-07-19 15:09:30 +0800

    add some code

--- src/notes/lisp1/src/lib.rs
+++ src/notes/lisp1/src/lib.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, hash::Hash};
+
 #[derive(Clone)]
 enum RispExp {
     Symbol(String),
@@ -9,10 +11,55 @@ enum RispErr {
     Reason(String),
 }
 
+#[derive(Clone)]
+struct RispEnv {
+    data: HashMap<String, RispExp>,
+}
+
+fn tokenize(expr: String) -> Vec<String> {
+    expr.replace("(", " ( ")
+        .replace(")", " ) ")
+        .split_whitespace()
+        .map(|x| x.to_string())
+        .collect()
+}
+
+fn parse<'a>(tokens: &'a [String]) -> Result<(RispExp, &'a [String]), RispErr> {
+    let (token, rest) = tokens
+        .split_first()
+        .ok_or(RispErr::Reason("could not get token".to_string()))?;
+    match &token[..] {
+        "(" => read_seq(rest),
+        ")" => Err(RispErr::Reason("unexpected `)`".to_string())),
+        _ => Ok((parse_atom(token), rest)),
+    }
+}
+
+fn read_seq<'a>(tokens: &'a [String]) -> Result<(RispExp, &'a [String]), RispErr> {
+    let mut res: Vec<RispExp> = vec![];
+    let mut xs = tokens;
+    loop {
+        let (next_token, rest) = xs
+            .split_first()
+            .ok_or(RispErr::Reason("could not find closing `)`".to_string()))?;
+        if next_token == ")" {
+            return Ok((RispExp::List(res), rest));
+        }
+        let (exp, new_xs) = parse(&xs)?;
+        res.push(exp);
+        xs = new_xs;
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_tokenize() {
+        let src = "(+ 10 5)";
+        let res = tokenize(src.into());
+        println!("{:?}", res);
+        assert!(false);
     }
 }
