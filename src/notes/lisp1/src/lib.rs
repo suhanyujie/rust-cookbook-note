f68d97c15699ae37f9423534b862e9a5c197ec48
suhanyujie <suhanyujie@qq.com>
2021-07-20 22:58:01 +0800

    add some code

--- src/notes/lisp1/src/lib.rs
+++ src/notes/lisp1/src/lib.rs
@@ -1,10 +1,11 @@
-use std::{collections::HashMap, hash::Hash};
+use std::{collections::HashMap, hash::Hash, num::ParseFloatError};
 
 #[derive(Clone)]
 enum RispExp {
     Symbol(String),
     Number(f64),
     List(Vec<RispExp>),
+    Func(fn(&[RispExp]) -> Result<RispExp, RispErr>),
 }
 
 enum RispErr {
@@ -49,6 +50,52 @@ fn read_seq<'a>(tokens: &'a [String]) -> Result<(RispExp, &'a [String]), RispErr
         res.push(exp);
         xs = new_xs;
     }
+}
+
+fn parse_list_of_floats(args: &[RispExp]) -> Result<Vec<f64>, RispErr> {
+    args.iter().map(|x| parse_single_float(x)).collect()
+}
+
+fn parse_single_float(exp: &RispExp) -> Result<f64, RispExp> {
+    match exp {
+        RispExp::Number(num) => Ok(*num),
+        _ => Err(RispErr::Reason("expect a number".to_string())),
+    }
+}
+
+fn parse_atom(token: &str) -> RispExp {
+    let potential_float: Result<f64, ParseFloatError> = token.parse();
+    match potential_float {
+        Ok(v) => RispExp::Number(v),
+        Err(_) => RispExp::Symbol(token.to_string().clone()),
+    }
+}
+
+fn default_env() -> RispEnv {
+    let mut data: HashMap<String, RispExp> = HashMap::new();
+    data.insert(
+        "+".to_string(),
+        RispExp::Func(|args: &[RispExp]| -> Result<RispExp, RispErr> {
+            let sum = parse_list_of_floats(args)?
+                .iter()
+                .fold(0.0, |sum, a| sum + a);
+            Ok(RispExp::Number(sum))
+        }),
+    );
+    data.insert(
+        "-".to_string(),
+        RispExp::Func(|args: &[RispExp]| -> Result<RispExp, RispErr> {
+            let floats = parse_list_of_floats(args)?;
+            let first = *floats
+                .first()
+                .ok_or(RispErr::Reason("expected at least one number".to_string()))?;
+            let sum_of_rest = floats[1..].iter().fold(0.0, |sum, a| sum + a);
+
+            Ok(RispExp::Number(first - sum_of_rest))
+        }),
+    );
+
+    RispEnv { data }
 }
 
 #[cfg(test)]
